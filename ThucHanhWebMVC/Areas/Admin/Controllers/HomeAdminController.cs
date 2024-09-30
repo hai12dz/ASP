187582f2fb01c02ad74f0b4cf05a03e1fe5ea3aa
hai12dz <fifahayrataolahai1762004@gmail.com>
2024-10-01 05:44:22 +0700

    video9

--- ThucHanhWebMVC/Areas/Admin/Controllers/HomeAdminController.cs
+++ ThucHanhWebMVC/Areas/Admin/Controllers/HomeAdminController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Azure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ThucHanhWebMVC.Models;
+using X.PagedList;
 
 namespace ThucHanhWebMVC.Areas.Admin.Controllers
 {
@@ -8,6 +12,7 @@ namespace ThucHanhWebMVC.Areas.Admin.Controllers
 
     public class HomeAdminController : Controller
     {
+        MasterContext db = new MasterContext();
         [Route("")]
         [Route("index")]
 
@@ -15,5 +20,27 @@ namespace ThucHanhWebMVC.Areas.Admin.Controllers
         {
             return View();
         }
+
+        [Route("danhmucsanpham")]
+        public IActionResult DanhMucSanPham(int? page)
+        {//list khong phan trang
+         //var lstSanPham = db.TDanhMucSps.ToList();
+         //return View(lstSanPham);
+
+
+            int pageSize = 12;
+            int pageNumber = page == null || page < 0 ? 1 : page.Value;
+            var lstsanpham = db.TDanhMucSps.AsNoTracking().OrderBy(x => x.TenSp);
+            PagedList<TDanhMucSp> lst = new PagedList<TDanhMucSp>(lstsanpham, pageNumber, pageSize);
+
+
+            return View(lst);
+         
+        }
+
+
+
+
+
     }
 }
